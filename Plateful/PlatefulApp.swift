22318234545d1e12a3f07370e55eae2e3b1cb684
Polina Sokolova <polly.sokolova@icloud.com>
2025-08-11 13:01:06 +0200

    Added Xcode project files

---
+++ Plateful/PlatefulApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatefulApp.swift
+//  Plateful
+//
+//  Created by Полина Соколова on 11.08.25.
+//
+
+import SwiftUI
+
+@main
+struct PlatefulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
